2d5dcef9617d7c9fb2ac0ca40fd14a2f9d681637
Moril <jeanmo333@gmail.com>
2023-02-25 00:05:52 -0300

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_HOST = 'https://api.themoviedb.org/3';
-
+export const API_KEY = process.env.API_KEY;
 export const LANG = 'es-ES';
 export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
